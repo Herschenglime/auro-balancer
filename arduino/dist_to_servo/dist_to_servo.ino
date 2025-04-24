8fbf27b78d1505f4de1ce07f5321ab6c1b4f3515
Herschenglime <herschenglime@gmail.com>
2025-04-23 20:53:47 -0400

    demo that maps distance to servo

---
+++ arduino/dist_to_servo/dist_to_servo.ino
@@ -0,0 +1,46 @@
+#include <Servo.h>
+#include "Adafruit_VL53L0X.h"
+
+Adafruit_VL53L0X lox = Adafruit_VL53L0X(); // dist sensor
+
+Servo myservo;  // create servo object to control a servo
+// twelve servo objects can be created on most boards
+
+int pos = 0;    // variable to store the servo position
+
+void setup() {
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+
+  Serial.begin(115200);
+
+  // wait until serial port opens for native USB devices
+  while (! Serial) {
+    delay(1);
+  }
+  
+  Serial.println("Adafruit VL53L0X test");
+  if (!lox.begin()) {
+    Serial.println(F("Failed to boot VL53L0X"));
+    while(1);
+  }
+  // power 
+  Serial.println(F("VL53L0X API Simple Ranging example\n\n")); 
+}
+
+
+void loop() {
+  VL53L0X_RangingMeasurementData_t measure;
+    
+  Serial.print("Reading a measurement... ");
+  lox.rangingTest(&measure, false); // pass in 'true' to get debug data printout!
+
+  if (measure.RangeStatus != 4) {  // phase failures have incorrect data
+    Serial.print("Distance (mm): "); Serial.println(measure.RangeMilliMeter);
+    pos = map(measure.RangeMilliMeter, 30, 400, 0, 180); 
+    myservo.write(pos);
+  } else {
+    Serial.println(" out of range ");
+  }
+    
+  delay(100);
+}
