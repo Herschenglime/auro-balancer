86d87c55b8b2f8a5e6b918bad983fba5f7b0fb0c
Qlopezscarim <herschenglime@gmail.com>
2025-04-26 17:25:01 -0400

    created skeleton ros package

---
+++ ros_ws/src/auro_balancer/auro_balancer/controller.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from auro_balancer.')
+
+
+if __name__ == '__main__':
+    main()
---
+++ ros_ws/src/auro_balancer/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'auro_balancer'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='robo',
+    maintainer_email='herschenglime@gmail.com',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'controller = auro_balancer.controller:main'
+        ],
+    },
+)
