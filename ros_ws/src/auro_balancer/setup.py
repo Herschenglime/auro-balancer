bacc8df8e0008048ec82011d4e65ff30d0a65b92
Herschenglime <qlopezscarim@ufl.edu>
2025-04-26 18:03:20 -0400

    finished package creation tutorial

--- ros_ws/src/auro_balancer/setup.py
+++ ros_ws/src/auro_balancer/setup.py
@@ -1,26 +1,23 @@
 from setuptools import find_packages, setup
 
-package_name = 'auro_balancer'
+package_name = "auro_balancer"
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='robo',
-    maintainer_email='herschenglime@gmail.com',
-    description='TODO: Package description',
-    license='Apache-2.0',
-    tests_require=['pytest'],
+    maintainer="robo",
+    maintainer_email="herschenglime@gmail.com",
+    description="Self-balancing ball seesaw for EEL5934 final.",
+    license="Apache-2.0",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
-            'controller = auro_balancer.controller:main'
-        ],
+        "console_scripts": ["controller = auro_balancer.controller:main"],
     },
 )
