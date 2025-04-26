d94333e6f6b4ecc243c5a28bda64a7b8abb18925
Qlopezscarim <qlopezscarim@ufl.edu>
2025-04-26 02:11:04 -0400

    fixed servo range by adjusting pwm width

--- drivers/servo/servo.py
+++ drivers/servo/servo.py
@@ -9,12 +9,22 @@ Device.pin_factory = PiGPIOFactory()
 # https://randomnerdtutorials.com/raspberry-pi-pwm-python/
 
 # using gpio 18 which is on pin 12, but the lib bases it on gpio num
-servo = Servo(18)
+servo = Servo(pin=18)
 
-val = -1
+val = 0
+up = True
+increment = 0.01
+sleep_time = 0.01
 
 # do a servo sweep
 try:
-    servo.mid()
+    print("initializing...")
+    while True:
+        servo.min()
+        sleep(0.5)
+        servo.mid()
+        sleep(0.5)
+        servo.max()
+        sleep(0.5)
 except KeyboardInterrupt:
     print("Program stopped")
